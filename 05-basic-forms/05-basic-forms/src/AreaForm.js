425c1e0d865f81e3974c08f164a6c5c15bdbe7f7
chevonnelise <elise.chevonne@gmail.com>
2024-03-26 07:23:17 +0000

    complete 05 answer

--- 05-basic-forms/05-basic-forms/src/AreaForm.js
+++ 05-basic-forms/05-basic-forms/src/AreaForm.js
@@ -1,28 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function AreaForm(){
-  
-    return (
-      <React.Fragment>
-        <div>
-          <label>Width:</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label>Height:</label>
-          <input type="text" />
-        </div>
-        <div>Area = </div>
-      </React.Fragment>
-    )
-  
-    const updateWidth =  (event) => {
-        
-    }
+export default function AreaForm() {
 
-    const updateHeight = (event) => {
-         
-    }
+  // 1. hook
+  const [width, setWidth] = useState("");
+  const [height, setHeight] = useState("");
+
+
+  // 2. initialization
+  const updateWidth = (event) => {
+    setWidth(event.target.value);
+  }
+
+  const updateHeight = (event) => {
+    setHeight(event.target.value);
+  }
+
+  // 3. calculate area
+  const area = width * height;
+
+  // 4. return jsx
+  return (
+    <React.Fragment>
+      <div>
+        <label>Width:</label>
+        <input type="text" value={width} onChange={updateWidth} />
+      </div>
+      <div>
+        <label>Height:</label>
+        <input type="text" value={height} onChange={updateHeight} />
+      </div>
+      <div>Area = {area}</div>
+    </React.Fragment>
+  )
 
 }
 
