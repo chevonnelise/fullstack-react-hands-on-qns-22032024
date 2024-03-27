2f7e0b6ec200788dd9647de2e306913be68d6e7b
chevonnelise <elise.chevonne@gmail.com>
2024-03-27 04:39:00 +0000

    complete 10

--- 10-buffet/10-buffet/src/Buffet.js
+++ 10-buffet/10-buffet/src/Buffet.js
@@ -1,32 +1,54 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 export default function Buffet() {
 
-    const [items, setItems] = useState([]);
+  const [items, setItems] = useState([]);
 
-    const calculateTotal = () => {
-        return 0;
+  const calculateTotal = () => {
+    const itemCosts = {
+      beef: 12.00,
+      chicken: 7.50,
+      fish: 10.00,
+      mushrooms: 5.50,
+      spinach: 2.50
+    };
+
+    let total = 0;
+    for (let i = 0; i < items.length; i++) {
+      total += itemCosts[items[i]];
     }
+    return total;
 
-    return (
-      <React.Fragment>
-          <div>
-            <input type="checkbox" name="orders" value="beef"/><label>Shabu Shabu Beef</label>
-          </div>
-          <div>
-            <input type="checkbox" name="orders" value="chicken"/><label>Chicken Fillet</label>
-          </div>
-          <div>
-            <input type="checkbox" name="orders" value="fish"/><label>Fish Fillet</label>
-          </div>
-          <div>
-            <input type="checkbox" name="orders" value="mushrooms"/><label>Assorted mushhrooms</label>
-          </div>
-          <div>
-            <input type="checkbox" name="orders" value="spinach"/><label>Spinach</label>
-          </div>
-          <h1>Total:{calculateTotal()}</h1>
-      </React.Fragment>
-    )
+  }
+
+  const handleCheckboxChange = (event) => {
+    const { value } = event.target;
+    if (!items.includes(value)) {
+      setItems([...items, value]); 
+    } else {
+      setItems(items.filter(item => item !== value)); 
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <div>
+        <input type="checkbox" name="orders" value="beef" onClick={handleCheckboxChange} /><label>Shabu Shabu Beef</label>
+      </div>
+      <div>
+        <input type="checkbox" name="orders" value="chicken" onClick={handleCheckboxChange} /><label>Chicken Fillet</label>
+      </div>
+      <div>
+        <input type="checkbox" name="orders" value="fish" onClick={handleCheckboxChange} /><label>Fish Fillet</label>
+      </div>
+      <div>
+        <input type="checkbox" name="orders" value="mushrooms" onClick={handleCheckboxChange} /><label>Assorted mushhrooms</label>
+      </div>
+      <div>
+        <input type="checkbox" name="orders" value="spinach" onClick={handleCheckboxChange} /><label>Spinach</label>
+      </div>
+      <h1>Total:{calculateTotal()}</h1>
+    </React.Fragment>
+  )
 }
-  
+
