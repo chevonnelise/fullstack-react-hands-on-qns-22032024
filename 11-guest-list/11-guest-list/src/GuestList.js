6f470837b9b0a8ef7e1ed29155b4f47a36f58000
chevonnelise <elise.chevonne@gmail.com>
2024-03-27 04:49:17 +0000

    complete 11

--- 11-guest-list/11-guest-list/src/GuestList.js
+++ 11-guest-list/11-guest-list/src/GuestList.js
@@ -11,9 +11,24 @@ export default function GuestList() {
         ]
     )
 
+    const handleGuestChange = (event) => {
+        const newName = prompt('Enter the new guest name:');
+        if (newName !== null) { // Check if the prompt wasn't canceled
+            const updatedGuests = [...guests]; // Create a copy of the guests array
+            updatedGuests[event] = newName; // Update the name of the guest at the specified index
+            setGuests(updatedGuests); // Update the state with the modified array
+        }
+
+    }
+
     return (
         <React.Fragment>
-
+            <h1>Guest List</h1>
+            <ul>
+                {guests.map((guest,index)=> (
+                    <li key={index} onClick={() => handleGuestChange(1)}>{guest}</li>
+                ))}
+            </ul>
         </React.Fragment>
     )
 }
